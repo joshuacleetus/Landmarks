962b6ce522b903c2595b47dc7878447365557b39
jcleetus <97258556+jcleetus@users.noreply.github.com>
2022-12-26 20:11:17 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Joshua Cleetus on 12/26/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
